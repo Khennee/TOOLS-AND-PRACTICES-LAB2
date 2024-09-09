708827af1c9e75e0977a0ffe78f90bef23f1c881
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2024-09-09 17:50:20 +0800

    yummy

--- Alair.py
+++ Alair.py
@@ -1,11 +1,8 @@
 def HelloShun(): 
-<<<<<<< HEAD
     print("Hello Wd ajdajd adadad {222 add!")
     print("Hello World!")    
-=======
     print("Hello Wdadadd!")
     print("Hello qqq!")    
->>>>>>> ALAIR
 
 HelloShun()
 
